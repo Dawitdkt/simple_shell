141221ccda4c426b894f44505e3380c721418a2b
Kitesa <kitesamerga@gmail.com>
2022-06-15 03:17:53 -0700

    Fixed an error

--- aux_stdlib.c
+++ aux_stdlib.c
@@ -54,11 +54,11 @@ char *aux_itoa(int n)
 		n1 = n;
 	}
 
-	length--;
+	lenght--;
 	do {
 		*(buffer + lenght) = (n1 % 10) + '0';
 		n1 = n1 / 10;
-		length--;
+		lenght--;
 	}
 	while (n1 > 0)
 		;
